0f3012e116ad2d4d47c097c60f671aa05fd60a86
JamLab-Cording <72563627+JamLab-Cording@users.noreply.github.com>
2020-10-23 22:11:52 +0900

    Pause(2020.10.23)

--- crawling.py
+++ crawling.py
@@ -3,13 +3,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
-
 StockName = ""
 
-url = "https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=주식" + StockName
-req = requests.get(url, headers=headers)
+urlBasic1 = "https://search.daum.net/search?nil_suggest=sugsch&w=news&"
+urlBasic2 = "DA=GIQ&sq=&o=1&sugo=30&cluster=y&q="
+urlStockName = "삼성전자"
+req = requests.get(urlBasic1+urlBasic2+urlStockName)
 soup = BeautifulSoup(req.text, 'html.parser')
-for anchor in soup.select(".txt_sub"):
-    text = anchor.get_text()
-    print(text[0:6])
+for anchor in soup.select("div.coll_cont"):
+    print(anchor.get.text())
